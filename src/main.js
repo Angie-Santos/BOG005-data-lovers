b328fb9f8c2b224c27f84488a7bc09bb2e662098
Angie-Santos <santosangie79@gmail.com>
2022-08-05 11:38:51 -0500

    visualizar - responsive

--- src/main.js
+++ src/main.js
@@ -3,28 +3,40 @@ import data from './data/lol/lol.js'
 
 // visualizar data
 
-    const champions = Object.values(data.data)
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    visualData(data);
+});
+
+function visualData(data){
+    const champions = Object.values(data.data)
+    
     for (let i = 0; i < champions.length; i++) {
     
         // Almacenar nombre y titulo de cada campeón en variables
-        let nameChampion = champions[i].name
+        let nameChampion = (champions[i].name).toUpperCase()
         let titleChampion = champions[i].title
         let linkImgChampion = champions[i].splash
+        let rolChampion = champions[i].tags[0];
+        console.log(rolChampion);
         
         // Crear elementos HTML
         let newArticle = document.createElement('article')
         newArticle.className = 'card'
-
+        
+        newArticle.style.backgroundImage = `url(${linkImgChampion})`;
+    
         newArticle.innerHTML= 
-        `<img src='${linkImgChampion}' class='card__imagechampion'>
+        `<img src='/src/assets/img/${rolChampion}.webp' class='card__imgrol'>
         <div class='card__content'>
             <p class='card__content__name'>${nameChampion}</p> 
             <p class='card__content__title'>${titleChampion}</p>
         </div>`
-
+        
         let main = document.querySelector('main')
         main.appendChild(newArticle)
     
     }
+
+}
     
